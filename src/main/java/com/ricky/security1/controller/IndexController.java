3ddb9dd282202214689d1977fefcd69ba71ccef5
명호 <kimzzan1234@gmail.com>
2022-08-03 23:32:13 +0900

    시큐리티 1~2강

---
+++ src/main/java/com/ricky/security1/controller/IndexController.java
@@ -0,0 +1,54 @@
+package com.ricky.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping({"","/"})
+    public String index(){
+        // 머스테치 기본폴더 -> src/main/resources/
+        // 뷰리졸버 설정 : templates (prefix), .mustache(suffix)
+        return "index"; // scr/main/resources/templates/index.mustache
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
+    }
+
+    @GetMapping("/join")
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨!";
+    }
+
+}
